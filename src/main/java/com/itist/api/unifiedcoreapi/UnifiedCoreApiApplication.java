a582766d6a87401ed5120754ed398e022fe82f8f
iTiSt.K <yeppplus@gmail.com>
2024-03-25 23:19:18 +0900

    first commit!

---
+++ src/main/java/com/itist/api/unifiedcoreapi/UnifiedCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.itist.api.unifiedcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itist/api/unifiedcoreapi/UnifiedCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itist.api.unifiedcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
