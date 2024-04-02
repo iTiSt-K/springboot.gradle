83642378681df01de255f77bcd34a9dafc374ecc
kgi0412 <kgi0412@ybtour.com>
2024-04-02 18:04:45 +0900

    프로파일 코드 추가.

--- src/main/java/com/itist/api/unifiedcoreapi/UnifiedCoreApiApplication.java
+++ src/main/java/com/itist/api/unifiedcoreapi/UnifiedCoreApiApplication.java
@@ -3,11 +3,27 @@ package com.itist.api.unifiedcoreapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class UnifiedCoreApiApplication {
 
+    @Value("${spring.application.name}")
+    private String appName;
+ 
+	@Value("${spring.profiles.active}")
+    private String profile;
+
+    @Value("${test}")
+    private String test;
+
 	public static void main(String[] args) {
 		SpringApplication.run(UnifiedCoreApiApplication.class, args);
+		log.info(">>> Started [] !!!", appName);
 	}
 
+	@PostConstruct
+    private void start() {
+        log.debug("profile = []", profile);
+		log.debug("test = []", test);
+    }
 }
